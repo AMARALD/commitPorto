7f00efbfa6332addc6b92447ccd99bd20dbb7e14
maria miranda <mariajplmiranda@gmail.com>
2016-05-21 19:06:06 +0100

    menu

--- js/base.js
+++ js/base.js
@@ -30,12 +30,29 @@ var Helpers = (function () {
 	var hash = window.location.hash,
 		nav = document.getElementsByTagName('nav')[0],
 		navHeight  = parseInt(Helpers.style(nav, 'height'), 10);
-
+	
 	window.onscroll = function() {
-		if(document.body.scrollTop >=  window.innerHeight - navHeight) {
-			Helpers.removeClass(nav, 'bottom');
-		} else {
-			Helpers.addClass(nav, 'bottom');
-		}
+		var scrolled  = window.scrollY; 
+
+		// if(document.body.scrollTop >=  window.innerHeight - navHeight) {
+		// 	Helpers.removeClass(nav, 'hidden');
+		// } else {
+		// 	Helpers.addClass(nav, 'hidden');
+		// }
+
+		// $('#title').css({
+  //           'transform': 'translate3d(0, ' + -(scrolled * 0.2) + 'px, 0)', // parallax (20% scroll rate)
+  //           'opacity': 1 - scrolled / 400 // fade out at 400px from top
+  //       });
+
+		// document.getElementById('nav').style.transform = 
+		// 	'translate3d(0, ' + (scrolled * 0.50)+ 'px, 0)';
+
+		//PASSAR A FAZER ISTO POR CLASSES 
+		document.getElementById('nav').style.opacity = 
+			scrolled / 400;
+
+        document.getElementById('video').style.transform = 
+        	'translate3d(0, ' + (scrolled * 0.50)+ 'px, 0)'; 
 	};
 })();
